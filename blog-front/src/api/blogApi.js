8597d5d0f1c45e8e7485ea955366704e0c89fd46
Makoto Henmi <mako.mail.g@gmail.com>
2018-08-19 10:27:21 +0900

    APIエンドポイントを環境毎に切り替えるように変更

--- blog-front/src/api/blogApi.js
+++ blog-front/src/api/blogApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = process.env.BLOG_API_ENDPOINT;
 
 const securedAxiosInstance = axios.create({
   baseURL: API_URL,
